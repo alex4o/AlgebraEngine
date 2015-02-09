df9deff9a207075325cbad52b314390290e242b1
Kosyo <3c3code@gmail.com>
2015-02-09 17:32:57 +0200

    RNG improvements, CoefDescriptor changed and entered use

---
+++ 7klas.cpp
@@ -0,0 +1,6 @@
+#include "7klas.hpp"
+
+void createEquivalentExpressions(Expression& e1, Expression& e2, ExpressionDescriptor& ed)
+{
+
+}
--- RNG.hpp
+++ RNG.hpp
@@ -3,6 +3,8 @@
 
 #include <ctime>
 #include <stdlib.h>
+#include "Number.hpp"
+#include "CoefDescriptor.hpp"
 
 using namespace std;
 
@@ -29,6 +31,27 @@ public:
     int nextInt(int low, int high)
     {
         return rand()%(high-low+1) + low;
+    }
+
+    Number nextNumber(CoefDescriptor& cd)
+    {
+        int r = nextInt(1, 100);
+        int sign = 1;
+        int r2 = nextInt(1, 100);
+        if(r2<=cd.pNegative) sign = -1;
+
+        if(r<=cd.pNatural)
+        {
+            return Number(sign*nextInt(cd.upLow, cd.upHigh));
+        }
+        else if(r>cd.pNatural and r<= cd.pRational)
+        {
+            return Number(sign*nextInt(cd.upLow, cd.upHigh), nextInt(cd.downLow, cd.downHigh));
+        }
+        else
+        {
+            return Number();
+        }
     }
 };
 
