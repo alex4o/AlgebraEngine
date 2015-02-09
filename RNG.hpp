276fdc6078150794dda9351b2905463685357e16
alex4o <mrabonin@gmail.com>
2015-02-09 17:36:38 +0200

    Added random_device

--- RNG.hpp
+++ RNG.hpp
@@ -1,35 +1,76 @@
 #ifndef RNG
 #define RNG
 
-#include <ctime>
-#include <stdlib.h>
+
+
+
+#ifdef linux
+	#include <random>
+
+
+#elif _WIN32
+	#include <ctime>
+	#include <stdlib.h>
+
+#else
+		
+#endif
 
 using namespace std;
 
 class RNJ
 {
+private:
+
+#ifdef linux
+	std::random_device rd("/dev/urandom");
+#endif
 
 public:
-    void init()
-    {
-        srand(time(0));
-    }
+	void init()
+	{
+	#ifdef _WIN32
+		srand(time(0));
+	#endif
+	}
 
 
-    RNJ()
-    {
-        init();
-    }
+	RNJ()
+	{
+		#ifdef linux
 
-    bool nextBool()
-    {
-        return rand()%2;
-    }
+		#elif _WIN32
+			init();
+		#else
+		
+		#endif
+		
+	}
 
-    int nextInt(int low, int high)
-    {
-        return rand()%(high-low+1) + low;
-    }
+	bool nextBool()
+	{
+		#ifdef linux
+			std::uniform_int_distribution<int> dist(0,1);
+			return dist(this.rd);
+		#elif _WIN32
+			return rand()%2;
+		#else
+
+		#endif
+	}
+
+	int nextInt(int low, int high)
+	{
+		#ifdef linux
+			std::uniform_int_distribution<int> dist(low,high);
+			return dist(this.rd);
+		#elif _WIN32
+			return rand()%(high-low+1) + low;
+		#else
+		
+		#endif
+
+	}
 };
 
 #endif
