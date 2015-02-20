9bec5239dbffe0e0a4051b9b8ea4509dfe7c6172
= <=>
2015-02-20 15:33:47 +0000

    segfault error

--- Interface.cpp
+++ Interface.cpp
@@ -10,26 +10,27 @@ using namespace std;
 extern "C"
 {
 
-	Result oprosti(ExpressionDescriptor ed)
+	void oprosti(ExpressionDescriptor ed, Result *res)
 	{
-		Result res;
 
 		Expression e1,e2;
 		createEquivalentExpressions(e1, e2, ed);
 		stringstream ss1, ss2;
 		e1.print(ss1);
 		e2.print(ss2);
-
 		string s1 = ss1.str();
 		string s2 = ss2.str();
+		cout<<s1<<"\n"<<s2<<"\n";
 
-		res.problem = (char*)malloc(s1.size() + 1);
-		res.solution = (char*)malloc(s2.size() + 1);
 
-		strcpy(res.problem, (const char*)s1.c_str());
-		strcpy(res.solution, (const char*)s2.c_str());
+		res->problem = (char*)malloc(s1.size() + 1);
+		res->solution = (char*)malloc(s2.size() + 1);
 
-		return res;
+		
+
+		strcpy(res->problem, s1.c_str());
+		strcpy(res->solution, s2.c_str());
+		cout<<res->problem<<"\n"<<res->solution<<"\n";
 	}
 }
 
