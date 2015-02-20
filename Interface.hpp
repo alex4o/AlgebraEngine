309ff5a021cc931df927682a255884dc11821710
= <=>
2015-02-20 14:54:14 +0000

    added Interface header

--- Interface.hpp
+++ Interface.hpp
@@ -13,27 +13,7 @@ extern "C"
         char* solution;
     };
 
-    Result oprosti(ExpressionDescriptor ed)
-    {
-        Result res;
-
-        Expression e1,e2;
-        createEquivalentExpressions(e1, e2, ed);
-        stringstream ss1, ss2;
-        e1.print(ss1);
-        e2.print(ss2);
-
-        string s1 = ss1.str();
-        string s2 = ss2.str();
-
-        res.problem = (char*)malloc(s1.size() + 1);
-        res.solution = (char*)malloc(s2.size() + 1);
-
-        strcpy(res.problem, (const char*)s1.c_str());
-        strcpy(res.solution, (const char*)s2.c_str());
-
-        return res;
-    }
+    Result oprosti(ExpressionDescriptor ed);
 }
 
 
