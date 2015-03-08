1e1f3a685b8e07856db4b68fc934a03a82d63426
alex4o <mrabonin@gmail.com>
2015-03-08 12:09:17 +0200

    now can pow and sqrt numbers

--- Number.cpp
+++ Number.cpp
@@ -5,297 +5,343 @@ using namespace std;
 
 int gcd ( int a, int b ) //Само се обяснява
 {
-    if(a<0) a=-a;
-    if(b<0) b=-b;
-    int c;
-    while ( a != 0 )
-    {
-        c = a;
-        a = b%a;
-        b = c;
-    }
-    return b;
+	if(a<0) a=-a;
+	if(b<0) b=-b;
+	int c;
+	while ( a != 0 )
+	{
+		c = a;
+		a = b%a;
+		b = c;
+	}
+	return b;
+}
+
+int expo(int a, int b){
+	int result = 1;
+
+	while (b){
+		if (b&1){
+			result *= a;
+		}
+	b >>=1 ;
+	a *= a;
+	}
+
+	return result;
 }
 
 Number::Number() //Стандартен конструктор. Нулира по образец на C#
 {
-    null=true;
-    fraction.up=0;
-    fraction.down=1;
-    type= natural | rational;
+	null=true;
+	fraction.up=0;
+	fraction.down=1;
+	type= natural | rational;
 }
 
 Number::Number(int n) //Конструктор за естествено число
 {
-    type = natural | rational; //Задава числото като рационално и естествено
-    if( n)null=false;     //тези 3 реда се обясняват сами
-    else null=true;
-    fraction.down=1;
-    fraction.up=n;
+	type = natural | rational; //Задава числото като рационално и естествено
+	if( n)null=false;     //тези 3 реда се обясняват сами
+	else null=true;
+	fraction.down=1;
+	fraction.up=n;
 }
 
 Number::Number(int _up, int _down) //Конструктор за дроб, работи аналогично на горния
 {
-    type=rational;
+	type=rational;
 
-    int g = gcd(_up, _down);
-    if(g>1)
-    {
-        _up/=g;
-        _down/=g;
-    }
+	int g = gcd(_up, _down);
+	if(g>1)
+	{
+		_up/=g;
+		_down/=g;
+	}
 
-    if(_down==1) type=type | natural; //Така е по-удобно, защото функциите трябва да викат само този конструктор
-    if(_up) null=false;
-    else null=true;
-    fraction.up=_up;
-    fraction.down=_down;
+	if(_down==1) type=type | natural; //Така е по-удобно, защото функциите трябва да викат само този конструктор
+	if(_up) null=false;
+	else null=true;
+	fraction.up=_up;
+	fraction.down=_down;
+}
+
+Number::Number(int num, Fraction pow)
+{                                    //Трябва се оправи в бъдеще
+	type=irrational;
+	null=false;
+	this->pow=pow;
+	fraction.up=num;
+	fraction.down=1;
+}
+
+bool Number::isPrime()
+{
+	if(this->isNatural()){
+		return (expo(2,fraction.up)-2)%fraction.up;
+	}
 }
 
 
 
 Number parseNum(string s)
 {
-    //cout<<"[parse]\n";
+	//cout<<"[parse]\n";
 
-    int sign = 1;
-    int idx=0;
-    bool slash = false;
-    if(s[0]=='-')
-    {
-        idx++;
-        sign=-1;
-    }
-    if(s[0]=='+') idx++;
+	int sign = 1;
+	int idx=0;
+	bool slash = false;
+	if(s[0]=='-')
+	{
+		idx++;
+		sign=-1;
+	}
+	if(s[0]=='+') idx++;
 
-    Number c;
-    c.fraction.up = 0;
-    while(s[idx]>='0' and s[idx]<='9')
-    {
-        c.fraction.up*=10;
-        c.fraction.up+=s[idx]-'0';
-        idx++;
-    }
+	Number c;
+	c.fraction.up = 0;
+	while(s[idx]>='0' and s[idx]<='9')
+	{
+		c.fraction.up*=10;
+		c.fraction.up+=s[idx]-'0';
+		idx++;
+	}
 
-    if(s[idx]=='/') idx++;
-    else if(idx==s.size())
-    {
-        //cout<<"\t returning natural\n";
-        c.fraction.up*=sign;
-        c.fraction.down=1;
-        c.type= natural | rational;
-        c.null=false;
-        return c;
-    }
+	if(s[idx]=='/') idx++;
+	else if(idx==s.size())
+	{
+		//cout<<"\t returning natural\n";
+		c.fraction.up*=sign;
+		c.fraction.down=1;
+		c.type= natural | rational;
+		c.null=false;
+		return c;
+	}
 
-    c.fraction.down=0;
-    while(s[idx]>='0' and s[idx]<='9')
-    {
-        c.fraction.down*=10;
-        c.fraction.down+=s[idx]-'0';
-        idx++;
-    }
+	c.fraction.down=0;
+	while(s[idx]>='0' and s[idx]<='9')
+	{
+		c.fraction.down*=10;
+		c.fraction.down+=s[idx]-'0';
+		idx++;
+	}
 
-    if(c.fraction.down==0)
-    {
-        //cout<<"\t returning invalid(x/0)\n";
-        return Number();
-    }
-    int g = gcd(c.fraction.up, c.fraction.down);
-    if(g!=1)
-    {
-        c.fraction.up/=g;
-        c.fraction.down/=g;
-    }
+	if(c.fraction.down==0)
+	{
+		//cout<<"\t returning invalid(x/0)\n";
+		return Number();
+	}
+	int g = gcd(c.fraction.up, c.fraction.down);
+	if(g!=1)
+	{
+		c.fraction.up/=g;
+		c.fraction.down/=g;
+	}
 
-    c.fraction.up*=sign;
+	c.fraction.up*=sign;
 
-    if(c.fraction.up==0)
-    {
-        //cout<<"\t returning 0\n";
-        c.type=natural;
-        c.null=true;
-        return c;
-    }
-    if(c.fraction.down==1)
-    {
-        //cout<<"\t returning natural(x/1)\n";
-        c.type=natural || rational;
-        c.null=false;
-        return c;
-    }
+	if(c.fraction.up==0)
+	{
+		//cout<<"\t returning 0\n";
+		c.type=natural;
+		c.null=true;
+		return c;
+	}
+	if(c.fraction.down==1)
+	{
+		//cout<<"\t returning natural(x/1)\n";
+		c.type=natural || rational;
+		c.null=false;
+		return c;
+	}
 
-    //cout<<"\t returning natural\n";
-    c.type= rational;
-    c.null=false;
-    return c;
+	//cout<<"\t returning natural\n";
+	c.type= rational;
+	c.null=false;
+	return c;
 }
 
 void Number::print(bool ignoreOne, bool ignoreSign, stringstream& ss)
 {
-    bool negative = false;
-    if(fraction.up<0) negative=true;
+	bool negative = false;
+	if(fraction.up<0) negative=true;
 
-    if(ignoreSign and negative) fraction.up*=-1;
-    if(ignoreOne and fraction.up==1 and !negative) return;
-    if(isNatural()) ss<<fraction.up;
-    else if(isRational())
-    {
-        if(negative and !ignoreSign)ss<<"-\\frac{"<<-fraction.up<<"}{"<<fraction.down<<"}";
-        else ss<<"\\frac{"<<fraction.up<<"}{"<<fraction.down<<"}";
-    }
-    if(ignoreSign and negative) fraction.up*=-1;
-    //cout<<" ["<<null<<isRational()<<isNatural()<<"] ";
-    //cout<<fraction.up<<"/"<<fraction.down;
+	if(ignoreSign and negative) fraction.up*=-1;
+	if(ignoreOne and fraction.up==1 and !negative) return;
+	if(isNatural()) ss<<fraction.up;
+	else if(isRational())
+	{
+		if(negative and !ignoreSign)ss<<"-\\frac{"<<-fraction.up<<"}{"<<fraction.down<<"}";
+		else ss<<"\\frac{"<<fraction.up<<"}{"<<fraction.down<<"}";
+	}else{
+		ss<<"value: "<<fraction.up<<"/"<<fraction.down
+		<<" root:"<<pow.down<<"\n";
+	}
+	if(ignoreSign and negative) fraction.up*=-1;
+	//cout<<" ["<<null<<isRational()<<isNatural()<<"] ";
+	//cout<<fraction.up<<"/"<<fraction.down;
 }
 
 
 Number operator+(const Number &n1, const Number &n2) //Събиране на 2 числа
 {
-    /*cout<<"[Sum] ";
-    n1.print();
-    cout<<" and ";
-    n2.print();
-    cout<<endl;*/
-    if(n1.null and n2.null) //Оптимизации ако 1 или 2 числа са 0
-    {
-        //cout<<"\t both are null\n";
-        return Number();
-    }
-    if(n1.null and !n2.null) return n2;
-    if(n2.null and !n1.null) return n1;
+	/*cout<<"[Sum] ";
+	n1.print();
+	cout<<" and ";
+	n2.print();
+	cout<<endl;*/
+	if(n1.null and n2.null) //Оптимизации ако 1 или 2 числа са 0
+	{
+		//cout<<"\t both are null\n";
+		return Number();
+	}
+	if(n1.null and !n2.null) return n2;
+	if(n2.null and !n1.null) return n1;
 
-    if(n1.isNatural() and n2.isNatural()) //Ако и двете числа са естествени
-    {
-        //cout<<"\t both are natural\n";
-        return Number(n1.fraction.up+n2.fraction.up);
-    }
-    if(n1.isRational() and n2.isRational()) //Ако и двете са рационални
-    {
-        //cout<<"\t both are fractions\n";
-        int u = n1.fraction.up*n2.fraction.down+n2.fraction.up*n1.fraction.down;
-        int d = n1.fraction.down*n2.fraction.down;
-        //cout<<"\t raw: "<<u<<"/"<<d<<endl;
-        int g = gcd(u,d);
-        u/=g;
-        d/=g;
-        return Number(u,d); //Ето за това говорех в този тип конструктор. Понякога d=1 но няма нужда да правим проверка тук
-    }
-    //cout<<"\t we shouldn't be here\n";
-    return Number(); //Часта за ирационални числа
+	if(n1.isNatural() and n2.isNatural()) //Ако и двете числа са естествени
+	{
+		//cout<<"\t both are natural\n";
+		return Number(n1.fraction.up+n2.fraction.up);
+	}
+	if(n1.isRational() and n2.isRational()) //Ако и двете са рационални
+	{
+		//cout<<"\t both are fractions\n";
+		int u = n1.fraction.up*n2.fraction.down+n2.fraction.up*n1.fraction.down;
+		int d = n1.fraction.down*n2.fraction.down;
+		//cout<<"\t raw: "<<u<<"/"<<d<<endl;
+		int g = gcd(u,d);
+		u/=g;
+		d/=g;
+		return Number(u,d); //Ето за това говорех в този тип конструктор. Понякога d=1 но няма нужда да правим проверка тук
+	}
+	//cout<<"\t we shouldn't be here\n";
+	return Number(); //Часта за ирационални числа
 }
 
 void Number::operator+=(const Number &n1) //Добавяне
 {
-    /*cout<<"Old: ";
-    print();
-    cout<<"\nAdding: ";
-    n1.print();*/
+	/*cout<<"Old: ";
+	print();
+	cout<<"\nAdding: ";
+	n1.print();*/
 
-    if(n1.null) return;
+	if(n1.null) return;
 
-    if(n1.isRational()) //Ако и двете числа са естествени
-    {
-        int nu = fraction.up*n1.fraction.down + n1.fraction.up*fraction.down;
-        int nd = fraction.down*n1.fraction.down;
+	if(n1.isRational()) //Ако и двете числа са естествени
+	{
+		int nu = fraction.up*n1.fraction.down + n1.fraction.up*fraction.down;
+		int nd = fraction.down*n1.fraction.down;
 
-        if(nd<0) nu*=-1;
+		if(nd<0) nu*=-1;
 
-        int g = gcd(nu, nd);
-        fraction.up = nu/g;
-        fraction.down = nd/g;
-        fraction.up=nu;
-        fraction.down=nd;
-    }
+		int g = gcd(nu, nd);
+		fraction.up = nu/g;
+		fraction.down = nd/g;
+		fraction.up=nu;
+		fraction.down=nd;
+	}
 
-    null=false;
+	null=false;
 
-    /*cout<<"\nResult: ";
-    print();*/
+	/*cout<<"\nResult: ";
+	print();*/
 
-    //cout<<"\t we shouldn't be here\n";
-    return; //Часта за ирационални числа
+	//cout<<"\t we shouldn't be here\n";
+	return; //Часта за ирационални числа
 }
 
 void Number::operator-=(const Number &n1) //Добавяне
 {
-    /*cout<<"[Sum] ";
-    n1.print();
-    cout<<" and ";
-    n2.print();
-    cout<<endl;*/
-    if(n1.null) return;
+	/*cout<<"[Sum] ";
+	n1.print();
+	cout<<" and ";
+	n2.print();
+	cout<<endl;*/
+	if(n1.null) return;
 
-    if(n1.isRational()) //Ако и двете числа са естествени
-    {
-        int nu = fraction.up*n1.fraction.down - n1.fraction.up*fraction.down;
-        int nd = fraction.down*n1.fraction.down;
+	if(n1.isRational()) //Ако и двете числа са естествени
+	{
+		int nu = fraction.up*n1.fraction.down - n1.fraction.up*fraction.down;
+		int nd = fraction.down*n1.fraction.down;
 
-        if(nd<0) nu*=-1;
+		if(nd<0) nu*=-1;
 
-        int g = gcd(nu, nd);
-        fraction.up = nu/g;
-        fraction.down = nd/g;
-        fraction.up=nu;
-        fraction.down=nd;
-    }
+		int g = gcd(nu, nd);
+		fraction.up = nu/g;
+		fraction.down = nd/g;
+		fraction.up=nu;
+		fraction.down=nd;
+	}
 
-    //cout<<"\t we shouldn't be here\n";
-    return; //Часта за ирационални числа
+	//cout<<"\t we shouldn't be here\n";
+	return; //Часта за ирационални числа
 }
 
 Number operator-(const Number &n1, const Number &n2) //Изваждането, всичко е аналогично на събирането
 {
-    if(n1.null and n2.null)
-    {
-        return Number();
-    }
-    if(n1.null and !n2.null) return Number(-n2.fraction.up, n2.fraction.down);
-    if(n2.null and !n1.null) return n1;
+	if(n1.null and n2.null)
+	{
+		return Number();
+	}
+	if(n1.null and !n2.null) return Number(-n2.fraction.up, n2.fraction.down);
+	if(n2.null and !n1.null) return n1;
 
-    if(n1.isNatural() and n2.isNatural())
-    {
-        return Number(n1.fraction.up-n2.fraction.up);
-    }
-    if(n1.isRational() and n2.isRational())
-    {
-        int u = n1.fraction.up*n2.fraction.down - n2.fraction.up*n1.fraction.down;
-        int d = n1.fraction.down*n2.fraction.down;
-        int g = gcd(u,d);
-        u/=g;
-        d/=g;
-        return Number(u, d);
-    }
+	if(n1.isNatural() and n2.isNatural())
+	{
+		return Number(n1.fraction.up-n2.fraction.up);
+	}
+	if(n1.isRational() and n2.isRational())
+	{
+		int u = n1.fraction.up*n2.fraction.down - n2.fraction.up*n1.fraction.down;
+		int d = n1.fraction.down*n2.fraction.down;
+		int g = gcd(u,d);
+		u/=g;
+		d/=g;
+		return Number(u, d);
+	}
 
-    return Number();
+	return Number();
 }
 
 Number operator*(const Number &n1, const Number &n2) //Умножението
 {
-    if(n1.null or n2.null) return Number(); //Оптимизации с 0
-    int u = n1.fraction.up*n2.fraction.up; //Доста по-просто от събирането
-    int d = n1.fraction.down*n2.fraction.down; //За разлика от събирането, тук дори не е заделено място за
-    int g = gcd(u, d);                         //ирационални числа
-    u/=g;
-    d/=g;
-    //std::cout<<"\t\t----[num*] "<<n1.fraction.up<<"/"<<n1.fraction.down<<" * "<<n2.fraction.up<<"/"<<n2.fraction.down<<" = "<<u<<"/"<<d<<std::endl;
-    return Number(u, d);
+	if(n1.null or n2.null) return Number(); //Оптимизации с 0
+	int u = n1.fraction.up*n2.fraction.up; //Доста по-просто от събирането
+	int d = n1.fraction.down*n2.fraction.down; //За разлика от събирането, тук дори не е заделено място за
+	int g = gcd(u, d);                         //ирационални числа
+	u/=g;
+	d/=g;
+	//std::cout<<"\t\t----[num*] "<<n1.fraction.up<<"/"<<n1.fraction.down<<" * "<<n2.fraction.up<<"/"<<n2.fraction.down<<" = "<<u<<"/"<<d<<std::endl;
+	return Number(u, d);
 }
 
 Number operator/(const Number &n1, const Number &n2) //Делението, аналогично на умножението
 {
-    if(n1.null or n2.null) return Number();
-    int u = n1.fraction.up*n2.fraction.down;
-    int d = n1.fraction.down*n2.fraction.up;
-    int g = gcd(u, d);
-    u/=g;
-    d/=g;
+	if(n1.null or n2.null) return Number();
+	int u = n1.fraction.up*n2.fraction.down;
+	int d = n1.fraction.down*n2.fraction.up;
+	int g = gcd(u, d);
+	u/=g;
+	d/=g;
 
-    if(d<0)
-    {
-        d*=-1;
-        u*=-1;
-    }
+	if(d<0)
+	{
+		d*=-1;
+		u*=-1;
+	}
 
-    return Number(u, d);
+	return Number(u, d);
+}
+
+
+
+void Number::operator ^=(int k)
+{
+	fraction.up = expo(fraction.up,k);
+	fraction.down = expo(fraction.down,k);
+}
+
+Number Number::operator ^(int k)
+{
+	return Number(expo(fraction.up,k),expo(fraction.down,k));
 }
--- Number.hpp
+++ Number.hpp
@@ -6,9 +6,12 @@
 #include <string>
 #include <sstream>
 #include <iostream>
+#include <cmath>
 
 #define natural 0x1
 #define rational 0x2
+#define irrational 0x4
+
 
 using namespace std;
 
@@ -18,105 +21,150 @@ class Number          //Класа за "математическо число"
 {                     //В момента поддържа само рационални числа, но в бъдеще ще трябва да поддържа и корени
 public:               //Което може да е леко гадно, но само леко(надявам се)
 
-    bool null; //ако числото е 0. Оптимизации
-    char type; //Побитово пазим типа на числото
-    Fraction fraction; //Дробта, която представлява числото - ако е естествено, знаменателя е 1
+	bool null; //ако числото е 0. Оптимизации
+	char type; //Побитово пазим типа на числото
+	Fraction fraction; //Дробта, която представлява числото - ако е естествено, знаменателя е 1
 
-    std::vector<Number> roots;      //Ирационалната част
-    std::vector<Fraction> root_pow; //за бъдеще
+	Fraction root;  //Ирационалната част
+	Fraction pow;   //за в бъдеще
 
-    Number();
+	Number();
 
-    Number(int n);
+	Number(int n);
 
-    Number(int _up, int _down);
+	Number(int _up, int _down);
 
-    /*Number(Number root, Fraction pow)  //А това е конструктора за ирационални числа
-    {                                    //Трябва се оправи в бъдеще
-        type=0;
-        null=false;
-        roots.push_back(root);
-        root_pow.push_back(pow);
-        fraction.up=0;
-        fraction.down=1;
-    }*/
+	Number(int num, Fraction root);  //А това е конструктора за ирационални числа
 
-    bool isNatural() const     //Тези 2 функции се обясняват сами
-    {
-        return type&natural;
-    }
+	bool isNatural() const     //Тези 3 функции се обясняват сами
+	{
+		return type&natural;
+	}
 
-    bool isRational() const
-    {
-        return type&rational;
-    }
+	bool isRational() const
+	{
+		return type&rational;
+	}
 
-    void operator+=(const Number &n1);
-    void operator-=(const Number &n1);
+	bool isIrrational() const
+	{
+		return type&irrational;
+	}
 
-    bool operator==(int k)
-    {
-        if(k==0) return fraction.up==0;
-        if(isNatural()) return fraction.up==k;
-        return false;
-    }
+	bool isPrime();
 
-    bool operator==(const Number& n1)
-    {
-        if(null and n1.null) return true;
-        return type==n1.type and fraction.up==n1.fraction.up and fraction.down==n1.fraction.down;
-    }
+	void sroot(){
+	int out_root = 1;
+	int in_root = 1;
+	for(int i  = 2; i * i <= fraction.up; ++i) //no sqrt, please
+	{
+		int count=0;
+		while(fraction.up%i == 0) //while, not if
+		{
+			count++;
+			cout<<"factor: "<<i<<"\n";
+			fraction.up/=i;
+		}
+		
+		if(count%2){
+			count-=1;
+			in_root *= i;
+		}
+		while(count){
+			count-=2;
+			out_root *= i;
 
-    bool operator!=(int k)
-    {
-        if(k==0) return fraction.up!=0;
-        if(isNatural()) return fraction.up!=k;
-        return true;
-    }
+		}
+	}
+	if(fraction.up != 1)
+	{
+		//factors.push_back(fraction.up);
+		cout<<"factor: "<<fraction.up<<"\n";
 
-    void operator=(int k)
-    {
-        fraction.up=k;
-        fraction.down=1;
-        type=natural| rational;
-        if(k) null=false;
-    }
+	}
+	
+	fraction.up *= in_root;
 
-    void operator*=(int k)
-    {
-        fraction.up*=k;
-        int g = gcd(fraction.up, fraction.down);
-        if(g==1) return;
-        fraction.up/=g;
-        fraction.down/=g;
-        if(fraction.down==1) type = rational | natural;
-    }
+	cout<<"out of root: "<<out_root<<"\n";
+	if(fraction.up == 1){
+		fraction.up = out_root;
+	}
 
-    void operator*=(const Number& n)
-    {
+	//fraction.up = sqrt(fraction.up);
+	fraction.down = sqrt(fraction.down);
 
-        fraction.up*=n.fraction.up;
-        fraction.down*=n.fraction.down;
-        int g = gcd(fraction.up, fraction.down);
-        if(g!=1)
-        {
-            fraction.up /= g;
-            fraction.down /= g;
-        }
+	}
 
-        if(fraction.down==1) type = rational | natural;
-        else type = rational;
-        if(fraction.up==0) null=true;
+	void operator+=(const Number &n1);
+	void operator-=(const Number &n1);
 
-    }
+	bool operator==(int k)
+	{
+		if(k==0) return fraction.up==0;
+		if(isNatural()) return fraction.up==k;
+		return false;
+	}
 
-    void print(bool ignoreOne, bool ignoreSign, stringstream& ss);
+	bool operator==(const Number& n1)
+	{
+		if(null and n1.null) return true;
+		return type==n1.type and fraction.up==n1.fraction.up and fraction.down==n1.fraction.down;
+	}
 
-    char getSign()
-    {
-        if(fraction.up<0) return '-';
-        return '+';
-    }
+	bool operator!=(int k)
+	{
+		if(k==0) return fraction.up!=0;
+		if(isNatural()) return fraction.up!=k;
+		return true;
+	}
+
+	void operator=(int k)
+	{
+		fraction.up=k;
+		fraction.down=1;
+		type=natural| rational;
+		if(k) null=false;
+	}
+
+	void operator*=(int k)
+	{
+		fraction.up*=k;
+		int g = gcd(fraction.up, fraction.down);
+		if(g==1) return;
+		fraction.up/=g;
+		fraction.down/=g;
+		if(fraction.down==1) type = rational | natural;
+	}
+
+	void operator*=(const Number& n)
+	{
+
+		fraction.up*=n.fraction.up;
+		fraction.down*=n.fraction.down;
+		int g = gcd(fraction.up, fraction.down);
+		if(g!=1)
+		{
+			fraction.up /= g;
+			fraction.down /= g;
+		}
+
+		if(fraction.down==1) type = rational | natural;
+		else type = rational;
+		if(fraction.up==0) null=true;
+
+	}
+
+	void operator ^=(int k);
+	Number operator ^(int k);
+
+
+	void print(bool ignoreOne, bool ignoreSign, stringstream& ss);
+
+	char getSign()
+	{
+		if(fraction.up<0) return '-';
+		return '+';
+	}
 };
 
 Number operator+(const Number &n1, const Number &n2);
