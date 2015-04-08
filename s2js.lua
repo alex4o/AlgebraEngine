185144e7a661cda9a238b121c00c25d9bee9f272
alex4o <mrabonin@gmail.com>
2015-04-08 15:46:51 +0300

    s2js.lua: bad support for webidl

--- s2js.lua
+++ s2js.lua
@@ -4,10 +4,12 @@ local lpeg = require("lpeg")
 
 lpeg.locale(lpeg) 
 
-function maybe(p) return p^-1 end
+
 
 setmetatable(_ENV or _G, { __index = lpeg or require"lpeg" })
-  
+
+function maybe(p) return p^-1 end
+
 local space = lpeg.space^0
 -- wtf space
 local br = (P"{" + P"}")
@@ -16,11 +18,17 @@ local text = R("az","AZ")^1
 
 local number = R"09"
 
-local type = P"struct" + P"class"
+local ti = P"struct" + P"class"
 -- type of data
 local sep = S";" * space
 -- end of line 
-local var = C(text*maybe(P("[")*space*(text+number+P'_')^1*space*P("]")+space*P"*"))*space*(C(text)*maybe(S",")*space)^0*sep
+local var = C(
+  text*maybe(space*P"*")
+  )*space*(
+    C(text)
+    *maybe(P("[")*space*C(number^0)*space*P("]"))
+    *maybe(S",")*space
+  )^0*sep
 -- get the struct properties 
 local tag = S'"<>' 
 --include definition tags
@@ -34,18 +42,26 @@ handle:close()
 structs = match(
   Ct(space*
   Ct(
-    C(type^1)* space * C(text)* space * br * Ct(Ct(space*var*space)^0) * br*space * sep * space
+    C(ti^1)* space * C(text)* space * br * Ct(Ct(space*var*space)^0) * br*space * sep * space
   )^1),result)
 
+type_c = {
+  int = "long",
+  bool = "boolean",
+  char = "byte",
+}
+json = false
+-- json output
 t = {}
+if json then
 for k,struct in pairs(structs) do
   --print(struct[2])
   t[struct[2]] = {}
   for k,prop in pairs(struct[3]) do
-    local type
+    local ti
     for k,var in pairs(prop) do
       if k == 1 then
-        type = var
+        ti = var
       else
         local prop = '{"type":"'..type..'", "var": "' ..var..'"}'
         table.insert(t[struct[2]],prop)
@@ -56,10 +72,56 @@ for k,struct in pairs(structs) do
 end
 
 for k,v in pairs(t) do
-  handler = io.open(k..".js","w")
+  handler = io.open(arg[2]..k..".js","w")
   array = "module.exports = ["..table.concat(v,',').."]"
   handler:write(array)
-  print(array)
+  print("Saveing "..k..".js at "..arg[2])
   
 end
+
+else
+--web idl
+
+
+for k,struct in pairs(structs) do
+  t[struct[2]] = {}
+  type_c[struct[2]] = k
+  for k,prop in pairs(struct[3]) do
+    local ti --tyepof prop
+    for k,var in pairs(prop) do
+      if k == 1 then
+        ti = var
+      else
+        --print(ti)
+        local pres = type_c[ti]
+        print(type(prop[k+1]))
+        if lpeg.match(number^1,type(k+1)) ~= nill then
+          pres = pres .. "["..prop[k+1].."]"..' '..var..';'
+          
+          --print(prop)
+        else
+          pres = pres..' '..var..';'
+        end
+        table.insert(t[struct[2]],pres)
+      end
+    end
+  end
+end
+
+for k,v in pairs(t) do
+  handler = io.open(arg[2]..k..".idl","w")
+  array = "interface "..k.." {\n"..table.concat(v,'\n').."\n}"
+  print(array)
+  print("Saveing "..k..".idl at "..arg[2])
+  
+end
+end
+
+ffi.cdef[[
+  struct a{
+    int b;
+  }
+]]
+
+a = ffi.new('struct a', 12)
 
