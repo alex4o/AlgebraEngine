c593735f77a4874b1f3a3631085ffd9277cb208b
3c3 <3c3code@gmail.com>
2015-12-02 18:56:13 +0200

    added FED(up with it)

---
+++ FracEquationDescriptor.h
@@ -0,0 +1,21 @@
+﻿#ifndef FEQDescriptor
+#define FEQDescriptor
+
+#include "RootDescriptor.hpp"
+#include "CoefDescriptor.hpp"
+
+struct FracEquationDescriptor
+{
+	int power;// за сега ще е 2
+	int maxVisualPower;
+	int minTransformations;
+	int maxTransformations;
+	char letter;
+	char genType; // 0 - гадно, 1 - по-малко гадно
+	RootDescriptor rd; // корени
+	CoefDescriptor bvd; // bad value descriptor
+	CoefDescriptor cf; // за украшения
+	//bool nice; не се използва за момента
+};
+
+#endif
